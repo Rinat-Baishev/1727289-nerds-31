4ff4d5a15198e24ad0919348851257b0f550a2ef
Rinat-Baishev <rinat281997@mail.ru>
2021-07-03 22:46:09 +0300

    Декоративные элементы новое внесение правок

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,32 @@
-const Link = document.querySelector(".modal-open-button");
-const Popup = document.querySelector(".modal-connection");
-const Close = Popup.querySelector(".modal-close");
+const loginLink = document.querySelector(".modal-open-button");
+const loginPopup = document.querySelector(".modal-connection");
+const loginClose = loginPopup.querySelector(".modal-close");
+const loginForm = loginPopup.querySelector(".connection-form");
+const loginLogin = loginPopup.querySelector(".login-name");
+const loginPassword = loginPopup.querySelector(".login-email");
 
-Link.addEventListener("click", function (evt) {
+loginLink.addEventListener("click", function (evt) {
   evt.preventDefault();
-  Popup.classList.add("modal-show");
+  loginPopup.classList.add("modal-show");
+  loginLogin.focus();
 });
 
-Close.addEventListener("click", function (evt) {
+loginClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  Popup.classList.remove("modal-show");
+  loginPopup.classList.remove("modal-show");
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!loginLogin.value || !loginPassword.value) {
+    evt.preventDefault();
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+    }
+  }
 });
